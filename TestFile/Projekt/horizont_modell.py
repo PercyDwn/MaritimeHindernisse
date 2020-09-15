34704c963221c46807a47d215855014c79499bc1
Matheus Schelp Langendyk <matheus.slangendyk@gmail.com>
2020-09-15 11:12:04 +0200

    horizont_modell Entwurf

--- TestFile/Projekt/horizont_modell.py
+++ TestFile/Projekt/horizont_modell.py
@@ -3,13 +3,20 @@ import numpy as np
 from numpy.linalg import multi_dot
 from ObjectHandler import *
 import random 
+from numpy import linalg as lin
 
 #Externe Variablen
 ObjectHandler = ObjectHandler()
+
+#Ordner evtl. anpassen!
+ObjectHandler.setImageFolder('Bilder/list1')
+ObjectHandler.setImageBaseName('')
+ObjectHandler.setImageFileType('.jpg')
+ObjectHandler.setDebugLevel(2)
 N = 5
 Q_horizon = [[0.1,0],
             [0,0.1]] ##Varianz des Modellrauschens Horizont
-R_horizont = 1 
+R_horizon = 1 
 P_horizon = [[1,0],
             [0,1]]
 
@@ -18,17 +25,21 @@ H_horizon = [[1,0],
 F_horizon = [[1,0],
             [0,1]] #Systemmatrix Horizont
 
-
-
 #k-schleife
 def horizonState_gnn (ObjectHandler,N,R_horizon,P_horizon,H_horizon,F_horizon):
-    pictures_availiable = True
+    pictures_available = True
     k = 0
     horizon_list = []
+    est_hor_k = np.zeros((1,2))
+    state_hor_meas = np.zeros((1,2))
+
     while pictures_available == True:
         try:
              ObjectHandler.updateObjectStates()
-             horizon_lines_k = ObjectHandler.getHorizon(k) #3 Horizont Kandidaten
+             HORIZON = ObjectHandler.getHorizonData(k) #3 Horizont Kandidaten
+             horizon_lines_k = HORIZON[0]
+             heightsDiff_horizon = np.zeros((len(horizon_lines_k)))
+             
         except InvalidTimeStepError as e:
                print(e.args[0])
                k = 0 
@@ -39,20 +50,27 @@ def horizonState_gnn (ObjectHandler,N,R_horizon,P_horizon,H_horizon,F_horizon):
             #Datensatzladen
         if k==N:
             #MN Horizont aufrufen, um nur eine Linie von drei zu erhalten als Horizont-Kandidat
-            est_hor_k = mn_horizon(horizon_list) #Estimate horizon am Zeitschritt k
+           
+            est_hor_k[0,0] = horizon_lines_k[0].height #vorübergehend
+            est_hor_k[0,1] = horizon_lines_k[0].angle #vorübergehend
+            
+            #est_hor_k = mn_horizon(horizon_list,N,M) #Estimate horizon am Zeitschritt k
         
         if k>=N:
-            est_hor_k,P_horizon = kalman_filter_prediction(est_hor_k, P_horizon,F_horizon,Q_horizon) # Kalman Prädiktion
+            est_hor_k,P_horizon = kalman_filter_prediction(np.transpose(est_hor_k), P_horizon,F_horizon,Q_horizon) # Kalman Prädiktion
             H_heigth = [[1,0],
                         [0,0]] #Matrix extraiert nur die Höhe des Horizonts
-            height_horizon = getHeight(horizon_lines_k)#Liste von Höhen aus Liste von Horizonten
-            horizon_opt = min(abs(height_horizon-multi_dot([H_height,est_hor_k]))) #Horizon mit dem kleinsten Höheunterschied als optimal wählen
+            for horizon in range(len(horizon_lines_k)):
+                heightsDiff_horizon[horizon] = abs(horizon_lines_k[horizon].height -est_hor_k[0])#Liste von Höhendifferenz verglichen mit der Höhe des Zustandsvektors
+     
+            state_hor_meas[0,0] =  horizon_lines_k[np.argmin(heightsDiff_horizon)].height #Höhe des nahligenden Horizonts
+            state_hor_meas[0,1] =  horizon_lines_k[np.argmin(heightsDiff_horizon)].angle  #Winkel des nahligenden Horizonts
             if len(horizon_lines_k) == 0:# Analysieren ob Horizon detektiert wurde
                 theta_k = 0
             else:
                 theta_k =1
-            est_hor_k,P_horizon = kalman_filter_update(est_hor_k,P_horizon,H_horizon,horizon_opt,theta_k,R_horizon,2)#Kalmann Update
-
+            est_hor_k,P_horizon = kalman_filter_update(est_hor_k,P_horizon,H_horizon,np.transpose(state_hor_meas),theta_k,R_horizon,2)#Kalmann Update
+            est_hor_k = np.transpose(est_hor_k)
             #Zustände auf gesamten Zustand zusammenführen
         k = k+1
 
@@ -82,7 +100,7 @@ def horizonState_phd (ObjectHandler,R_horizon,P_horizon,H_horizon,F_horizon):
            theta_k = 0
         else:
            theta_k =1
-        est_hor_k,P_horizon = kalman_filter_update(est_hor_k,P_horizon,H_horizon,horizon_opt,theta_k,R_horizon,2)#Kalmann Update
+        est_hor_k,P_horizon = kalman_filter_update(np.transpose(est_hor_k),P_horizon,H_horizon,horizon_opt,theta_k,R_horizon,2)#Kalmann Update
         #Zustände auf gesamten Zustand zusammenführen
 
         k = k+1
@@ -124,3 +142,5 @@ def kalman_filter_update(estimate_i,P_i,H,z_opt_assossiation,theta_i,R,dimension
          estimate_i = estimate_i
      
      return estimate_i,P_i
+
+horizonState_gnn (ObjectHandler,N,R_horizon,P_horizon,H_horizon,F_horizon)
