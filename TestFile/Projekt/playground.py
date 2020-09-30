e6f5b7fe1e0877731d8c2d728a3c7d0ef1d94fce
Nevermann <derneverich@gmail.com>
2020-09-30 12:48:55 +0200

    push to leave

--- TestFile/Projekt/playground.py
+++ TestFile/Projekt/playground.py
@@ -126,7 +126,7 @@ def phd_BirthModels (num_w: int, num_h: int) -> List[Gaussian]:
     b_area: List[Gaussian] = []
     for i in range(1,num_h):
         for j in range(1, num_w): 
-            mean = vstack([j*obj_w/(num_h+1), i*obj_h/(num_h+1), 0.0, 0.0])
+            mean = vstack([j*obj_w/(num_w+1), i*obj_h/(num_h+1), 0.0, 0.0])
             b_area.append(Gaussian(mean, cov_edge))
     
     
