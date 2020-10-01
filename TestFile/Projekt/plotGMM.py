d3052c9462a1958fa429991fd48bfb8516a7cd09
Percy Downing <percy.downing@icloud.com>
2020-10-01 17:35:35 +0200

    improve progress display

--- TestFile/Projekt/plotGMM.py
+++ TestFile/Projekt/plotGMM.py
@@ -23,18 +23,29 @@ def plotGMM(gmm: list, pixel_w: int, pixel_h: int, detail: int = 1) -> plt:
   
   gaussian_count = len(gmm)
   gaussian_counter = 0
+  pixel_row_count = gaussian_count * rows
+  pixel_row_counter = 0
   print('analyse gaussians (' + str(gaussian_count) + ' total)')
   for gaussian_obj in gmm:
     gaussians_analysed_percent = cast(int, gaussian_counter / gaussian_count * 100)
-    stdout.write("\r%d percent finished" % gaussians_analysed_percent)
-    stdout.flush()
-    # print("gaussian " + str(gaussian_counter) + " of " + str(gaussian_count))
     distribution = gaussian_obj.distribution()
     for i in range(1,rows, detail):
       for j in range(1, cols, detail):
         gz[i,j] +=  gaussian_obj.distributionValue(distribution, vstack([i, j, 0, 0]))
+      pixel_row_counter+=(1*detail)
+      pixel_rows_analysed_percent = cast(float,(pixel_row_counter / pixel_row_count) * 100)
+      if detail >=5:
+        stdout.write("\r%.0f percent finished    " % pixel_rows_analysed_percent)
+      elif detail < 5 and detail > 2:
+        stdout.write("\r%.1f percent finished    " % pixel_rows_analysed_percent)
+      else:
+        stdout.write("\r%.2f percent finished    " % pixel_rows_analysed_percent)
+
+      
+      stdout.flush()
+        
     gaussian_counter+=1
-  stdout.write("\r100 percent finished")
+  stdout.write("\r100 percent finished  ")
   stdout.flush()
   stdout.write("\n")
 
